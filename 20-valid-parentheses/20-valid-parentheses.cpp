b875881252830cf93a6694c5d9bb5f3012f44148
MEHA--SHUKLA <75724727+MehaShukla@users.noreply.github.com>
2022-04-25 15:03:21 +0530

    Time: 5 ms (17.74%), Space: 6.4 MB (10.19%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-    
-    for(int idx = 0; idx < s.size(); idx++){
-        char ch = s[idx];
-        
-        if(st.size() > 0){
-        if(ch == '(' || ch == '{' || ch == '[')
-            st.push(ch);
-        
-        else if (ch == ')' && st.top() == '(')
-            st.pop();
-        
-         else if (ch == '}' && st.top() == '{')
-            st.pop();
-        
-        else if (ch == ']' && st.top() == '[')
-            st.pop();
-            else
-                st.push(ch);
-        
-        }
-       else if(ch == '(' || ch == '{' || ch == '[' || ch == ')' || ch == '}' || ch == ']')
-       st.push(ch);
-        
-    }
-    
-    if(st.size() == 0)
-        return true;
-    else 
+        int n= s.length();
+        stack <int> st;
+        for(int i=0;i<n;i++)
+        {
+           if(st.size()>0)
+           {
+               if(s[i]=='['||s[i]=='{'||s[i]=='(')
+                   st.push(s[i]);
+               else if(s[i]==')'&&st.top()=='(')
+                   st.pop();
+               else if(s[i]==']'&&st.top()=='[')
+                   st.pop();
+               else if(s[i]=='}'&&st.top()=='{')
+                   st.pop();
+               else
+                  st.push(s[i]);  
+           }
+            else if(s[i]=='['||s[i]==']'||s[i]=='('||s[i]==')'||s[i]=='{'||s[i]=='}')
+                st.push(s[i]);
+        }if(st.size()==0)
+            return true;
+        else
         return false;
+        
     }
 };
