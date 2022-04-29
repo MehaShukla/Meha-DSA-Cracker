bd708a57c0adc30a240f872356f93c81a3e63a3b
MEHA--SHUKLA <75724727+MehaShukla@users.noreply.github.com>
2022-04-30 02:44:21 +0530

    Time: 0 ms (100.00%), Space: 6 MB (28.01%) - LeetHub

---
+++ 461-hamming-distance/461-hamming-distance.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int c=0;
+        while(x!=0||y!=0)
+        {
+            if((x&1)!=(y&1))
+                c++;
+            x=x>>1;
+            y=y>>1;
+        }
+        return c;
+            
+    }
+};
