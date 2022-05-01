f41a246cbe60dc31b271c76ad8b2530af0d61a59
MEHA--SHUKLA <75724727+MehaShukla@users.noreply.github.com>
2022-05-01 19:06:59 +0530

    Time: 137 ms (33.85%), Space: 59 MB (39.26%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -2,24 +2,12 @@ class Solution {
 public:
     int maxArea(vector<int>& height) {
         int n=height.size();
-      int left [n];
-        left[0]=height[0];
-        int right[n];
-        right[n-1]=height[n-1];
-        for(int i=1;i<n;i++)
-        {
-            left[i]=max(left[i-1],height[i]);
-        }
-        for(int i=n-2;i>=0;i--)
-        {
-            right[i]=max(right[i+1],height[i]);
-        }
         int i=0,j=n-1,mx=INT_MIN;
         while(i<j)                          //two pointers concept
         {
-            int area=min(left[i],right[j])*(j-i);// area chye so beadth nikalre
+            int area=min(height[i],height[j])*(j-i);// area chye so beadth nikalre
             mx=max(mx,area);
-            if(left[i]>right[j])
+            if(height[i]>height[j])
                 j--;
             else
                 i++;
