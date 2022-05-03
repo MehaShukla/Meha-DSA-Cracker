4e683282d8b600b398bb7c640c13fe9a147612a4
MEHA--SHUKLA <75724727+MehaShukla@users.noreply.github.com>
2022-05-03 16:22:24 +0530

    Time: 4 ms (50.58%), Space: 6 MB (18.31%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
-    int fib(int n) {
-      if(n==0||n==1)
-          return n;
-        return fib(n-1)+fib(n-2);
+  int fib(int n) {
+        vector<int> dp(31, 0);
+        dp[0] = 0; dp[1] = 1;
+        for(int i=2; i<=30; i++)// space time both O(n)
+            dp[i]= dp[i-1] + dp[i-2];
+        return dp[n];
     }
 };
