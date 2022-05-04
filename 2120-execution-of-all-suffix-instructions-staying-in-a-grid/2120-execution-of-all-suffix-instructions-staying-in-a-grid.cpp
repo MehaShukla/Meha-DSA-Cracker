3ce88fe71ee00cc755067a89665e97d4bcdb5c8d
MEHA--SHUKLA <75724727+MehaShukla@users.noreply.github.com>
2022-05-04 13:42:50 +0530

    Time: 18 ms (97.62%), Space: 10.2 MB (86.34%) - LeetHub

--- 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.cpp
+++ 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.cpp
@@ -1,45 +1,37 @@
 class Solution {
 public:
-    int count(vector<int> start,int n,string s)
-    {
-        int steps = 0;
-        int i = start[0];
-        int j = start[1];
-        for(int k = 0;k<s.length();k++)
-        {
-            if(s[k]=='L'&&j>0)
-            {
-                steps++;
-                j--;
-            }
-            else if(s[k]=='R'&&j<n-1)
-            {
-                steps++;
-                j++;
-            }
-            else if(s[k]=='U'&&i>0)
-            {
-                steps++;
-                i--;
-            }
-            else if(s[k]=='D'&&i<n-1)
-            {
-                steps++;
-                i++;
-            }
-            else 
-            {
-                break;
-            }
-        }
-        return steps;
-    }
+
     vector<int> executeInstructions(int n, vector<int>& startPos, string s) {
-        vector<int> res;
-        for(int i = 0;i<s.length();i++)
-        {
-            res.push_back(count(startPos,n,s.substr(i)));
-        }
-        return res;
+       int len = s.size(), count = 0;
+            vector<int> ans(len);
+              for(int i = 0; i < len; i++){
+                int start_x = startPos[0];
+                int start_y = startPos[1];
+                int count = 0;
+                for(int j = i; j < len; j++){
+                    char ch = s[j];
+                    if(ch == 'R' && start_y+1 < n){
+                        start_y++;
+                        count++;
+                    }
+                    else if(ch == 'D' && start_x+1 < n){
+                        start_x++;
+                        count++;
+                    }
+                    else if(ch == 'L' && start_y-1 >= 0){
+                        start_y--;
+                        count++;
+                    }
+                    else if(ch == 'U' && start_x-1 >= 0){
+                        start_x--;
+                        count++;
+                    }
+                    else {
+                        break;
+                    }
+                }
+                ans[i] = count;
+            }
+            return ans;
     }
 };
