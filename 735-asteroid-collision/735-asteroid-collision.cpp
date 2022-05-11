60219dc4ff28999cd43b4574f98e9c352d56c533
MEHA--SHUKLA <75724727+MehaShukla@users.noreply.github.com>
2022-05-11 18:15:21 +0530

    Time: 29 ms (14.75%), Space: 17.5 MB (83.39%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.cpp
+++ 735-asteroid-collision/735-asteroid-collision.cpp
@@ -1,50 +1,15 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int> s;
-        vector<int> v;
-        for(int i = 0;i<asteroids.size();i++)
-        {
-            if(s.empty()||asteroids[i]>0)
-            {
-                s.push(asteroids[i]);
-            }
-            else
-            {
-                while(true)
-                {
-                    if(s.top()<0 && asteroids[i]<0)
-                    {
-                        s.push(asteroids[i]);
-                        break;
-                    }
-                    else if(s.top() == -asteroids[i])
-                    {
-                        s.pop();
-                        break;
-                    }
-                    else if(s.top()>-asteroids[i])
-                    {
-                        break;
-                    }
-                    else 
-                    {
-                        s.pop();
-                        if(s.empty())
-                        {
-                            s.push(asteroids[i]);
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-        while(!s.empty())
-        {
-            v.push_back(s.top());
-            s.pop();
-        }
-        reverse(v.begin(),v.end());
-        return v;
+       vector<int> res;
+	for(int a : asteroids) {
+		bool destroyed = false;
+		while(res.size() && res.back() > 0 && a < 0 && !destroyed) {
+			if(res.back() >= -a) destroyed = true;
+			if(res.back() <= -a) res.pop_back();
+		}
+		if(!destroyed) res.push_back(a);
+	}
+	return res;
     }
 };
