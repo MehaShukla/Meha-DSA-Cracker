939f37c59a784346561fbfee4e1b23503603b257
MEHA--SHUKLA <75724727+MehaShukla@users.noreply.github.com>
2022-05-29 15:33:00 +0530

    Time: 5 ms (40.00%), Space: 6.1 MB (20.00%) - LeetHub

---
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool digitCount(string num) {
+        int n=num.length();
+        map<char , int >m;
+       
+      for(long i=0;i<num.length();i++)
+        m[num[i]]++;
+        int j=0;
+      
+        for(auto i:m)
+        { if((i.second!=(num[(i.first)-'0']-'0')))
+               {
+                  
+                   return false;
+               }
+    }
+        return true;
+                
+        
+    }
+};
