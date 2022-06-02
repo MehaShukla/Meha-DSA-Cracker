67dc2b29f8877c4a7e1e85c51d0835033dc03e9e
MEHA--SHUKLA <75724727+MehaShukla@users.noreply.github.com>
2022-06-02 17:11:05 +0530

    Time: 545 ms (80.66%), Space: 112.8 MB (93.72%) - LeetHub

---
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& roads) {
+            vector<int> A(n,0);			//For counting the number of edges the given node is part of
+        for(int i=0;i<roads.size();i++)
+        {
+            A[roads[i][0]]++;
+            A[roads[i][1]]++;
+        }
+        sort(A.begin(),A.end());
+        long long ans=0;
+        for(int i=0;i<n;i++)
+        {
+            ans+=((long long)A[i]*(i+1));//cchota no should be multiplied by cchota indegree no to get max total imp
+        }
+        return ans;   
+    }
+};
