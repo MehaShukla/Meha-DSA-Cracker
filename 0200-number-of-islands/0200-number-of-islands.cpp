b13ef95069472962608d4366158e94883904281c
MEHA--SHUKLA <75724727+MehaShukla@users.noreply.github.com>
2023-11-23 20:57:56 +0530

    Time: 496 ms (5.01%), Space: 396.5 MB (5.06%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int ans = 0;
+        vector<vector<int>> vis(m,vector<int> (n,0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(!vis[i][j] && grid[i][j] == '1'){
+                    ans++;
+                    cout<<i<<" "<<j<<endl;
+                    bfs(i,j,vis,grid);
+                }
+            }
+        }
+        return ans;
+    }
+    
+    void bfs(int i, int j, vector<vector<int>>& vis,vector<vector<char>> grid){
+        
+        int m = grid.size();
+        int n = grid[0].size();
+        int drows[] = {-1,0,1,0};
+        int dcols[] = {0,1,0,-1};
+        queue<pair<int,int>> q;
+        q.push({i,j});
+        vis[i][j] = 1;
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            int row = it.first;
+            int col = it.second;
+            
+            for(int k=0;k<4;k++){
+                int nrow = row + drows[k];
+                int ncol = col + dcols[k];
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && !vis[nrow][ncol] && grid[nrow][ncol] == '1'){
+                    q.push({nrow,ncol});
+                    vis[nrow][ncol] = 1;
+                }
+            }
+        }
+    }
+};
