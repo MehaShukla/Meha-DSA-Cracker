e04b7879f668c42fea7c56293e4bdd9554aa2467
MEHA--SHUKLA <75724727+MehaShukla@users.noreply.github.com>
2023-11-24 15:08:20 +0530

    Time: 549 ms (7.46%), Space: 15 MB (48.59%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_set<ListNode*> st;
-        while(headA){
-            st.insert(headA);
-            headA = headA->next;
-        }
-        while(headB){
-            if(st.find(headB) != st.end()){
-                return headB;
+        ListNode* ptrA = headA;
+        ListNode* ptrB = headB;
+        while(ptrA != ptrB) {
+            ptrA = ptrA->next;
+            if(ptrA == NULL && ptrB != NULL) {
+                ptrB = ptrB->next;
+                ptrA = headA;
             }
-            headB = headB->next;
         }
-        return nullptr;
+        return ptrA;
     }
 };
+// class Solution {
+// public:
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//         unordered_set<ListNode*> st;
+//         while(headA){
+//             st.insert(headA);
+//             headA = headA->next;
+//         }
+//         while(headB){
+//             if(st.find(headB) != st.end()){
+//                 return headB;
+//             }
+//             headB = headB->next;
+//         }
+//         return nullptr;
+//     }
+// };
