8245229d9f385be35313e663d139b8c902788712
MEHA--SHUKLA <75724727+MehaShukla@users.noreply.github.com>
2023-11-30 15:26:12 +0530

    Time: 686 ms (7.83%), Space: 384.3 MB (5.32%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
-           int n = s.length();
+  int n = s.length();
     int i = 0;
 
-    for (int j = 0; j < n; ++j, ++i) {
+    for (int j = 0; j < n; ) {
         s[i] = s[j];
+        ++i;
 
         // If the current character is the same as the k-1 characters before it, remove them
-        if (i >= k - 1 && s.substr(i - k + 1, k) == std::string(k, s[i])) {
+        if (i >= k && s.substr(i - k, k) == std::string(k, s[i - 1])) {
             i -= k;
         }
+
+        ++j;
     }
 
     // Resize the string to remove any extra characters at the end
